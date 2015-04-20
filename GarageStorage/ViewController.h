ade93b49b832271541aa85945ade62b99ccd08c0
Sam Voigt <Sam@Sams-MacBook-Pro.local>
2015-04-20 16:11:48 -0400

    Initial Commit

---
+++ GarageStorage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GarageStorage
+//
+//  Created by Sam Voigt on 4/20/15.
+//  Copyright (c) 2015 Wellframe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GarageStorage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GarageStorage
+//
+//  Created by Sam Voigt on 4/20/15.
+//  Copyright (c) 2015 Wellframe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
